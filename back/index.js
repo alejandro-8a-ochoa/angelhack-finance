23a53f5ba64ed218c93a62b86908096211103f02
devcharles <charlesipn@gmail.com>
2019-07-13 13:34:19 -0500

    standart index

--- back/index.js
+++ back/index.js
@@ -1,14 +1,13 @@
-const { server, port} = require ('./src/server')
-const db = require ('./src/lib/db');
+const { server, port } = require('./src/server')
+const db = require('./src/lib/db')
 
- db.connect()
-   .then(()=>{
-     console.log('Base de datos conectada')
-     server.listen(port, ()=>{
-       console.log('servidor corriedo en el puerto: ', port)
-     })
-   })
-   .catch(error=>{
-     console.error('error: ', error)
-   })
-
+db.connect()
+  .then(() => {
+    console.log('Base de datos conectada')
+    server.listen(port, () => {
+      console.log('servidor corriedo en el puerto: ', port)
+    })
+  })
+  .catch(error => {
+    console.error('error: ', error)
+  })
